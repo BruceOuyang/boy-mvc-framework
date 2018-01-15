908c39765bfc66397d31f3a2e5051f98263ebde6
Bruce Ouyang <oiiopro@live.cn>
2018-01-16 01:35:00 +0800

    update

---
+++ source/boy-mvc/src/test/java/club/bugmakers/chapter2/test/CustomerServiceTest.java
@@ -0,0 +1,67 @@
+package club.bugmakers.chapter2.test;
+
+import club.bugmakers.chapter2.model.Customer;
+import club.bugmakers.chapter2.service.CustomerService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by BuGMaker'Bruce on 2018/1/16.
+ */
+public class CustomerServiceTest {
+
+    private final CustomerService customerService;
+
+    public CustomerServiceTest(){
+        customerService = new CustomerService();
+    }
+
+    @Before
+    public void init(){
+        // TODO init database
+    }
+
+    @Test
+    public void getCustomerListTest() throws Exception {
+        List<Customer> customerList = customerService.getCustomerList(null);
+        Assert.assertEquals(2, customerList.size());
+    }
+
+    @Test
+    public void getCustomerTest() throws Exception {
+        long id = 1;
+        Customer customer = customerService.getCustomer(id);
+        Assert.assertNotNull(customer);
+    }
+
+    @Test
+    public void createCustomerTest() throws Exception {
+        Map<String, Object> fieldMap = new HashMap<>();
+        fieldMap.put("name", "Crystal");
+        fieldMap.put("contact", "Jiang");
+        fieldMap.put("telephone", "-");
+        boolean result = customerService.createCustomer(fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void updateCustomerTest() throws Exception {
+        long id = 1;
+        Map<String, Object> fieldMap = new HashMap<>();
+        fieldMap.put("contact", "Ouyang");
+        boolean result = customerService.updateCustomer(id, fieldMap);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void deleteCustomerTest() throws Exception {
+        long id = 1;
+        boolean result = customerService.deleteCustomer(id);
+        Assert.assertTrue(result);
+    }
+}
