6d77fa2c52152580529507c12642d27e13500a75
Bruce Ouyang <oiiopro@live.cn>
2018-01-16 01:34:03 +0800

    add utils

---
+++ source/boy-mvc/src/main/java/club/bugmakers/util/StringUtil.java
@@ -0,0 +1,30 @@
+package club.bugmakers.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by BuGMaker'Bruce on 2018/1/16.
+ */
+public class StringUtil {
+
+    /**
+     * 判断字符串是否为空
+     * @param str
+     * @return
+     */
+    public static boolean isEmpty(String str) {
+        if(str != null){
+            str = str.trim();
+        }
+        return StringUtils.isEmpty(str);
+    }
+
+    /**
+     * 判断字符串是否非空
+     * @param str
+     * @return
+     */
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
+    }
+}
